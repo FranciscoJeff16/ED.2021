879b30472543c0f4aaaf1ea0d262abd381669ba8
FranciscoJeff16 <94660469+FranciscoJeff16@users.noreply.github.com>
2021-11-20 13:44:16 -0300

    Códigos - Trabalho AP1

--- Códigos Trabalho AP1/Torre_Hanoi_4_Pinos.cpp
+++ Códigos Trabalho AP1/Torre_Hanoi_4_Pinos.cpp
@@ -12,16 +12,13 @@ int main(){
     return 0;
 }
 void Hanoi_4_Pinos(int discos, char comeco, char aux_1, char aux_2, char fim){
-   // Caso só tenho um disco a mudança será só  A -> B
+   // Caso só tenho um disco a mudança será só  A -> D
     if ( discos == 1 ){
         cout << comeco << " -> " << fim << endl;
-   // Caso tenha dois discos a mudanã será  A -> B, A -> D e B -> D    
-    }else if ( discos == 2 ){
-        cout << comeco << " -> " << aux_1 << endl;
-        cout << comeco << " -> " << fim << endl;
-        cout << aux_1 << " -> " << fim << endl;
-    //Caso sejam mais que 2 discos
-    }else {
+        return;
+    }
+  // Para todos os outros casos    
+    if(discos > 1){
         Hanoi_4_Pinos(discos - 2, comeco, aux_2, fim, aux_1);
         cout << comeco << " -> " << aux_2 << endl;
         cout << comeco << " -> " << fim << endl;
