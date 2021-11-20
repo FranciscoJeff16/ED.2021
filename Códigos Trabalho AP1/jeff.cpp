8f0f33880aa675f8ead5c389229f0c00088ab0ba
FranciscoJeff16 <94660469+FranciscoJeff16@users.noreply.github.com>
2021-11-20 16:00:36 -0300

    Código teste

---
+++ Códigos Trabalho AP1/jeff.cpp
@@ -0,0 +1,13 @@
+# include <iostream> //´de entrada e saída
+#include <iomanip> //manipular saída
+#include <cmath>//biblioteca de matemática
+
+using namespace std;
+
+int main () {
+int d;
+cin >> d;
+cout << setw(20) << d << "\n";
+cout << setprecision(4) << sqrt(2);
+ return 0;
+ }
